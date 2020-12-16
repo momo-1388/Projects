3e29974fe3c0cf66a6c14a2f65459162b35cc01c
momo-1388 <mohamamd1eydi@gmail.com>
2020-12-17 00:17:04 +0330

    This 15.py

--- 15.py
+++ 15.py
@@ -1,16 +1,51 @@
 import requests
 import bs4
-import re
+import mysql.connector
+from sklearn import tree
 
-r = requests.get("https://emalls.ir/%D9%84%DB%8C%D8%B3%D8%AA-%D9%82%DB%8C%D9%85%D8%AA_%D9%84%D9%BE-%D8%AA%D8%A7%D9%BE~Category~40")
+sql = mysql.connector.connect(
+    user='root',
+    host='localhost',
+    password='13888',
+    database='web'
+)
+table = 'pro'
+cu = sql.cursor()
+
+prc = list()
+nac = list()
+
+r = requests.get('https://www.truecar.com/used-cars-for-sale/listings/?sort%5B%5D=best_match')
 
 soup = bs4.BeautifulSoup(r.text, 'html.parser')
 
-names = soup.find_all('a', attrs={'class': 'maintitle'})
-prices = soup.find_all('div', attrs={'class': 'col-md-2 col-sm-12 col-12 item-price'})
-for name in names:
-    print(name.text.strip())
+names = soup.find_all('span', attrs={'class': "vehicle-header-make-model text-truncate"})
+w_cars = soup.find_all('div', attrs={'data-test': "vehicleMileage"})
+k_cars = soup.find_all('div', attrs={'data-qa': "ConditionHistory"})
+maps = soup.find_all('div', attrs={'data-qa': "Location"})
+prices = soup.find_all('h4', attrs={'data-test': "vehicleCardPricingBlockPrice"})
+cars = list(zip(names, w_cars, k_cars, maps, prices))
+for name, kar1, kar2, map_, price in cars:
+    # cur = sql.cursor()
+    # cur.execute('INSERT INTO {} VALUES (%s, %s, %s, %s, %s)'.format(table),
+    #            (name.text, kar1.text, kar2.text, map_.text, price.text))
+    print('_________________________')
+    print('name : %s' % name.text)
+    print('kar : %s' % kar1.text)
+    print('karl : %s' % kar2.text)
+    print('map : %s' % map_.text)
+    print('price : %s' % price.text)
 
-for price in prices:
-    op = re.findall(r'\d', price.text)
-    print(''.join(op))
+cu.execute('SELECT * FROM {}'.format(table))
+my_res = cu.fetchall()
+
+for res in my_res:
+    nac.append(res[:4])
+    prc.append(res[4])
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(nac, prc)
+
+answer = clf.predict(input('Name : '), input('Kar1 : '), input('Kar2 : '), input('Map : '))
+print(answer)
+sql.commit()
